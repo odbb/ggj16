96489e1b628d5eae8924ebdf11394f721f13f263
Firtina Ozbalikci <firtina14@gmail.com>
2016-01-31 12:23:25 +0000

    Added splash screen

---
+++ UnityProject/Notify/Assets/OS/Scripts/Core/Splash.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Splash : MonoBehaviour
+{
+	public float alpha = 1;
+	public float minAlpha = 0.1f;
+	public float lerpFactor = 0.05f;
+	public float waitTime = 3.0f;
+
+	private bool fading = false;
+	// Use this for initialization
+	public IEnumerator Start ()
+	{
+		yield return new WaitForSeconds(waitTime);
+
+		fading = true;
+	}
+	
+	// Update is called once per frame
+	void Update ()
+	{
+		if (fading)
+		{
+			alpha = Mathf.Lerp(alpha, 0, lerpFactor);
+
+			GetComponent<CanvasGroup>().alpha = alpha;
+
+			if (alpha <= minAlpha)
+			{
+				Destroy(gameObject);
+			}
+		}
+	}
+}
